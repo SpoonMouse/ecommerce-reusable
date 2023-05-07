a7b7d77aabccce648fd9dd5d8449266becd8273d
Dominic Tracy <spoon@Dominics-MacBook-Air.local>
2023-05-06 22:08:34 -0500

    added native email/password sign-up functionality

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth  } from '../../utils/firebase/firebase.utils';
 
 const defaultFormFields = {
     displayName: '',
@@ -11,6 +12,33 @@ const SignUp = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if(password !== confirmPassword) {
+            alert("passwords do not match")
+            return;
+        }
+
+        try {
+            const { user } = await createAuthUserWithEmailAndPassword(email, password);
+            
+            await createUserDocumentFromAuth(user, { displayName });
+            resetFormFields();
+
+        } catch (error) {
+          if(error.code === 'auth/email-already-in-use') {
+            alert('Cannot create user, email already in use');
+          } else {
+            console.log(error)
+          }
+        }
+    }
+
     const handleChange = (e) => {
         const {name, value} = e.target;
 
@@ -19,7 +47,7 @@ const SignUp = () => {
   return (
     <div>
         <h1>Sign up with your email and password</h1>
-        <form onSubmit={() => {}}>
+        <form onSubmit={handleSubmit}>
             <label>Display Name</label>
             <input type='text'required onChange={handleChange} name='displayName' value={displayName}/>
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth';
 
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+import { getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyAmtUY8n4xUuX1Lo8xEPIK2sgsYwBmVF8s",
@@ -26,7 +26,8 @@ const firebaseConfig = {
 
   export const db = getFirestore()
 
-  export const createUserDocumentFromAuth = async (userAuth) => {
+  export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+    if(!userAuth) return;
     const userDocRef = doc(db, 'users', userAuth.uid);
 
     const userSnapshot = await getDoc(userDocRef);
@@ -39,7 +40,8 @@ const firebaseConfig = {
         await setDoc(userDocRef, {
           displayName, 
           email,
-          createdAt
+          createdAt,
+          ...additionalInformation,
         })
       } catch (error) {
         console.log('error creating the user', error.message);
@@ -48,3 +50,9 @@ const firebaseConfig = {
 
     return userDocRef;
   };
+
+  export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password)
+  }
