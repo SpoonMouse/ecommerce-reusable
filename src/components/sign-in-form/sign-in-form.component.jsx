73a9b4d78bd189c1a79d24ea4f487f19794c1188
SpoonMouse <tracyd153@gmail.com>
2023-05-15 20:37:29 -0500

    finished auth page component and sign-in-form

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -31,10 +31,20 @@ const SignInForm = () => {
         try {
           const response = await signInAuthUserWithEmailAndPassword(email, password)
             console.log(response)
-        } catch (error) {
 
-        }
-    }
+        } catch (error) {
+          switch(error.code) {
+            case 'auth/wrong-password':
+              alert('incorrect password for email');
+              break;
+            case 'auth/user-not-found':
+              alert('no user associated with this email');
+              break;
+             default:
+              console.log(error);
+          }
+        } 
+      };
 
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -62,7 +72,7 @@ const SignInForm = () => {
               value={password}/>
             <div className='buttons-container'>
               <Button type="submit">Sign In</Button>
-              <Button buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
+              <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
             </div>
         </form>
     </div>
